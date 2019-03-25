1726d2c0e281c3396d812ad05090292b7e40e6c5
BrigiK <kocs_brigitta@yahoo.com>
2019-03-25 09:17:14 +0200

    New insertion and print tests added.

--- BinarySearchTreeTests/BinarySearchTreeTests.cpp
+++ BinarySearchTreeTests/BinarySearchTreeTests.cpp
@@ -2,6 +2,8 @@
 #include "CppUnitTest.h"
 #include "BinarySearchTree.h"
 
+#include <sstream>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace BinarySearchTreeTests
@@ -38,6 +40,18 @@ namespace BinarySearchTreeTests
 			Assert::AreEqual(1, binSearchTree.size(), L"Value was not inserted, or size did not return correctly");
 		}
 
+		TEST_METHOD(InsertedFiveAndSizeIsFive)
+		{
+			BinarySearchTree binSearchTree;
+			binSearchTree.insert(5);
+			binSearchTree.insert(3);
+			binSearchTree.insert(6);
+			binSearchTree.insert(2);
+			binSearchTree.insert(4);
+
+			Assert::AreEqual(5, binSearchTree.size(), L"Value was not inserted, or size did not return correctly");
+		}
+
 		TEST_METHOD(Removed)
 		{
 			BinarySearchTree binSearchTree;
@@ -61,10 +75,15 @@ namespace BinarySearchTreeTests
 			BinarySearchTree binSearchTree;
 			int value = 10;
 
+			// MEGKELL CSINALNI HOGY NE LEHESSEN TOBB EGYFORMA SZAMOT BETENNI
+			binSearchTree.insert(value + 5);
+			binSearchTree.insert(value + 5);
+			binSearchTree.insert(value + 3);
+			binSearchTree.insert(value + 3);
 			binSearchTree.insert(value);
 			binSearchTree.insert(value);
 
-			Assert::AreEqual(1, binSearchTree.size());
+			Assert::AreEqual(3, binSearchTree.size());
 		}
 
 		TEST_METHOD(RemoveUnexistentValue)
@@ -78,6 +97,33 @@ namespace BinarySearchTreeTests
 			binSearchTree.insert(value + 5);
 			binSearchTree.remove(value);
 			Assert::AreEqual(1, binSearchTree.size());
+		}
+
+		TEST_METHOD(Print)
+		{
+			BinarySearchTree binSTree;
+			binSTree.insert(5);
+			binSTree.insert(6);
+			binSTree.insert(3);
+			binSTree.insert(4);
+			binSTree.insert(2);
+
+			std::stringstream strStream;
+			strStream << binSTree;
+			
+			std::string expected = "2 3 4 5 6 ";
+			Assert::AreEqual(expected, strStream.str());
+		}
+
+		TEST_METHOD(PrintEmpty)
+		{
+			BinarySearchTree binSTree;
+
+			std::stringstream strStream;
+			strStream << binSTree;
+
+			std::string expected = "";
+			Assert::AreEqual(expected, strStream.str());
 		}
 	};
 }
