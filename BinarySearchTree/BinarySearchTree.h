1f3e48769a629118d58dd8f9acd6398518694ab3
BrigiK <kocs_brigitta@yahoo.com>
2019-04-01 16:38:30 +0300

    Added destructor, copy constructor and copy assignment operator for Node.

--- BinarySearchTree/BinarySearchTree.h
+++ BinarySearchTree/BinarySearchTree.h
@@ -7,6 +7,9 @@ private:
 	struct Node
 	{
 		Node(int value) : value(value), left(nullptr), right(nullptr) {}
+		Node(const Node &node) = delete;
+		Node& operator=(const Node &node) = delete;
+		~Node();
 
 		int value;
 		Node* left;
@@ -16,13 +19,23 @@ private:
 public:
 	BinarySearchTree();
 
+	BinarySearchTree(const BinarySearchTree &binarySearchTree) = delete;
+
+	BinarySearchTree& operator=(const BinarySearchTree &binarySearchTree) = delete;
+	
+	~BinarySearchTree();
+
 	void insert(int value);
 
 	bool exists(int value);
 
+	Node* find(int value);
+
 	void remove(int value);
 
 	int size();
+
+	std::pair<Node*, Node*> min(Node* node, Node* parent);
 
 	friend void operator<<(std::ostream& os, const BinarySearchTree& binSearchTree);
 
