1130204d132a19992b0a08a8eaae085017243258
BrigiK <kocs_brigitta@yahoo.com>
2019-04-16 00:25:28 +0300

    Added main.

--- BinarySearchTree/Main.cpp
+++ BinarySearchTree/Main.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
-#include "BinarySearchTree.h"
 
 int main()
 {
-	BinarySearchTree binarySearchTree;
-	binarySearchTree.insert(9);
-	binarySearchTree.insert(15);
-	binarySearchTree.insert(21);
-	binarySearchTree.insert(2);
-	binarySearchTree.insert(4);
-	binarySearchTree.insert(6);
-	binarySearchTree.insert(14);
-	binarySearchTree.insert(1);
-
-	std::cout << binarySearchTree;
 	return 0;
 }
