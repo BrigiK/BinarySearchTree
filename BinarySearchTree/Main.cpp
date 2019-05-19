b16837355ddc678d91fee29af430a28f8eac28ba
BrigiK <kocs_brigitta@yahoo.com>
2019-05-19 19:08:33 +0300

    Updated main with the InheritanceCompletion function.

--- BinarySearchTree/Main.cpp
+++ BinarySearchTree/Main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <set>
+#include "CSquare.h"
+#include "CRectangle.h"
+#include "CCircle.h"
+#include "CShapeMap.h"
+#include "CShape.h"
+
+void InheritanceCompletion()
+{
+	CShapeMap map;
+
+	map.insert(new CSquare("My favorite square", /*width=*/10));
+	map.insert(new CRectangle("Some rectangle", /*l1=*/2, /*l2=*/3));
+	map.insert(new CCircle("Main circle", 20));
+	map.insert(new CCircle("Secondary circle", 21));
+
+	for(auto shape : map)
+	{
+		std::cout << "Name: " << shape->Name() << ", Area: " << shape->GetSurfaceArea() << std::endl;
+	}
+
+	auto favorite = map["My favorite square"];
+
+	std::cout << "favorite Name: " << favorite->Name() << ", favorite Area: " << favorite->GetSurfaceArea() << std::endl;
+
+	try
+	{
+		map.insert(new CSquare("My favorite square", /*width=*/20));
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl; //expected [Cannot add duplicate entry “My favorite square”]
+	}
+}
 
 int main()
 {
+	InheritanceCompletion();
 	return 0;
 }
